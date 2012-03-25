930fedf2e5bb645d182f3163dfa0b8ee23dc59ad
Henrik <henrik@newdawn.dk>
2012-03-25 20:09:20 +0200

    added cookie session handler to demo server

--- server/src/main/java/com/vonhof/webbeans/demo/DemoServer.java
+++ server/src/main/java/com/vonhof/webbeans/demo/DemoServer.java
@@ -5,6 +5,7 @@ import com.vonhof.babelshark.language.JsonLanguage;
 import com.vonhof.webi.FileRequestHandler;
 import com.vonhof.webi.Webi;
 import com.vonhof.webi.mvc.MVCRequestHandler;
+import com.vonhof.webi.session.CookieSessionHandler;
 import com.vonhof.webi.websocket.SocketService;
 import java.io.File;
 import java.sql.SQLException;
@@ -21,6 +22,8 @@ public class DemoServer {
         final Webi webi = new Webi(8081);
         webi.addBean(server);
         webi.add("/socket/data", new SocketService<DataClient>(DataClient.class));
+        
+        webi.add("/", new CookieSessionHandler("DEMO"));
         
         final MVCRequestHandler mvcHandler = webi.add("/rest/",new MVCRequestHandler());
         mvcHandler.expose(new DataController());
